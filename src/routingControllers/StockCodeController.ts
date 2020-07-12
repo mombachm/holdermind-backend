f213a7f75a9453c96fedd5af9a8283bcdc8bfa83
Matheus Mombach <matheus.mombach@sap.com>
2020-07-11 23:48:37 -0300

    Create StockCodeService

--- src/routingControllers/StockCodeController.ts
+++ src/routingControllers/StockCodeController.ts
@@ -1,10 +1,11 @@
 import * as express from "express";
 import { RoutingController } from "./abstractRoutingController/RoutingController";
 import { StockCodeService } from "../services/StockCodeService";
-import { StockCodeModel } from "../models/StockCode";
 
 export enum StockCodeControllerRoute {
-  GetUserStockCodes = "/getUserStockCodes"
+  GetUserStockCodes = "/getUserStockCodes",
+  CreateStockCodeForUser = "/createStockCodeForUser",
+  DeleteStockCodeFromUser = "/deleteStockCodeFromUser"
 }
 
 export enum StockCodeControllerParameter {}
@@ -13,6 +14,8 @@ export class StockCodeController extends RoutingController {
 
   protected assembleRoutes(): void {
     this.router.get(StockCodeControllerRoute.GetUserStockCodes, this.getUserStockCodes);
+    this.router.post(StockCodeControllerRoute.CreateStockCodeForUser, this.createStockCodeForUser);
+    this.router.post(StockCodeControllerRoute.DeleteStockCodeFromUser, this.deleteStockCodeFromUser);
   }
 
   public async getUserStockCodes(req: express.Request, res: express.Response): Promise<void> {
@@ -21,8 +24,12 @@ export class StockCodeController extends RoutingController {
   }
 
   public async createStockCodeForUser(req: express.Request, res: express.Response): Promise<void> {
-    const stockCode = new StockCodeModel(req.body);
-    const newStockCode = await stockCode.save();
+    const newStockCode = await StockCodeService.createUserStockCode(req.body.code);
     res.json(newStockCode);
+  }
+
+  public async deleteStockCodeFromUser(req: express.Request, res: express.Response): Promise<void> {
+    const deletedCont = await StockCodeService.deleteStockCodeFromUser(req.body.code);
+    res.json({ deleted: Boolean(deletedCont) });
   }
 }
--- src/services/StockCodeService.ts
+++ src/services/StockCodeService.ts
@@ -5,4 +5,13 @@ export class StockCodeService {
     const codes = await StockCodeModel.findAll();
     return codes;
   }
+
+  public static async createUserStockCode(stockCode: string): Promise<any> {
+    const stockCodeModel = new StockCodeModel({code: stockCode});
+    return await stockCodeModel.save();
+  }
+
+  public static async deleteStockCodeFromUser(stockCode: string): Promise<number> {
+    return StockCodeModel.destroy({where: {code: stockCode}});
+  }
 }
